e9e879342c0e14a27b981293d932a1b79ac0a5d0
yuebin.xie <yuebin.xie@qunar.com>
2015-07-20 13:30:31 +0800

    fds

--- HF_DP/src/guava/splitter/SplitterTest.java
+++ HF_DP/src/guava/splitter/SplitterTest.java
@@ -1,5 +1,10 @@
 package guava.splitter;
 
+import com.google.common.base.CharMatcher;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+
 /**
  * Created with IntelliJ IDEA.
  * User: yuebin.xie
@@ -8,5 +13,34 @@ package guava.splitter;
  * To change this template use File | Settings | File Templates.
  * Note:Splitter class test
  */
-public class SplitterTest {
+public class SplitterTest{
+
+    /*Splitter.on(char separator)*/
+    public static void onCharTest() {
+        Iterable<String> iterable = Splitter.on('s').split("apples,splitter");
+        printResult("onCharTest", iterable);
+    }
+
+    /* Splitter.on(final CharMatcher separatorMatcher) {*/
+    public static  void onCharMatcher() {
+        CharMatcher charMatcher = CharMatcher.DIGIT.and(CharMatcher.is('s'));//数字或者是s字符
+
+    }
+
+
+    private static void printResult(String methodName, Iterable<String> iterable) {
+        printResult(methodName, iterable, null);
+    }
+    private static void printResult(String methodName,Iterable<String> iterable, String iterableName) {
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(methodName), "methodName must not be null or empty");
+        Preconditions.checkArgument(iterable != null, "iterable must not be null");
+        for (String print : iterable) {
+            System.out.println(methodName + " " + Strings.nullToEmpty(iterableName) + ": [" + print + "]");
+        }
+    }
+
+    public static void main(String[] args) {
+        //add test method
+        onCharTest();
+    }
 }

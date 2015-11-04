61f684f0c5ad1a9667c590bf1277e674c59740e3
yuebin.xie <yuebin.xie@qunar.com>
2015-11-04 23:35:33 +0800

    chang package

--- HF_DP/src/main/java/com/max/tse/common/utils/SpringWrapper.java
+++ HF_DP/src/main/java/com/max/tse/common/utils/SpringWrapper.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
 /**
- * User: liuzz Date: 12-8-7 Time: 下午6:09
+ * User: yuebin.xie
  * 由于目前大部分的bean没有在spring中管理,导致无法注入，所以先使用一个wrapper适配一下
  */
 public class SpringWrapper implements ApplicationContextAware {

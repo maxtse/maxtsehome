fca886f36dc7532657dd9c591a000dc77a7766f9
解月彬 <xieyuebin@meituan.com>
2016-08-08 17:57:43 +0800

    fs

---
+++ HF_DP/src/main/java/com/max/tse/drools/DroolsServiceBuilder.java
@@ -0,0 +1,7 @@
+package com.max.tse.drools;
+
+/**
+ * Created by max on 8/6/16.
+ */
+public class DroolsServiceBuilder {
+}
---
+++ HF_DP/src/main/java/com/max/tse/drools/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by max on 8/6/16.
+ * drools规则引擎学习
+ */
+package com.max.tse.drools;
